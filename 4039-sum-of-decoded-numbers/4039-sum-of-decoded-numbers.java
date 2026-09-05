ff06957de70770adf9227b5551b0071dee59562e
Ashmita S A <ashmitaarumugam3@gmail.com>
2026-09-05 20:46:44 +0530

    Time: 99 ms (79.96%), Space: 80.9 MB (75.91%) - LeetHub

--- 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.java
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.java
@@ -1,26 +1,42 @@
 class Solution {
-    long power(long x, long y, long mod) {
-         if (y == 0)
-            return 1;
-       long half = power(x, y / 2, mod);
-            half = (half * half) % mod;
-        if (y % 2 == 1)
-            half = (half * x) % mod;
-            return half;
-    }
     public int sumDecoded(long[] nums) {
-        long mod = 1000000007;
-        long ans = 0;
-        int n = nums.length;
-         for (int i = 0; i < n; i++) {
-            String s = String.valueOf(nums[i]);
-            int width = (int)(nums[i] % 10);
-            String dst = s.substring(0, s.length() - 1);
-            int x = Integer.parseInt(dst.substring(0, width));
-            int y = Integer.parseInt(dst.substring(width));
-            long val = power(x, y, mod);
-            ans = (ans + val) % mod;
+
+        long MOD = 1000000007;
+        long total = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            long width = nums[i] % 10;
+            long d = nums[i] / 10;
+
+            int digits = String.valueOf(d).length();
+
+            long divisor = 1;
+
+            for (int j = 0; j < digits - width; j++) {
+                divisor *= 10;
+            }
+
+            long x = d / divisor;
+            long y = d % divisor;
+
+            // Calculate x^y % MOD
+            long res = 1;
+            long base = x;
+
+            while (y > 0) {
+
+                if (y % 2 == 1) {
+                    res = (res * base) % MOD;
+                }
+
+                base = (base * base) % MOD;
+                y = y / 2;
+            }
+
+            total = (total + res) % MOD;
         }
-      return (int)ans;
+
+        return (int) total;
     }
 }
